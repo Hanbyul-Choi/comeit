703a0b2bdb0fe264884558ea544ee737724b10ef
HyunseoY <maricat0715@gmail.com>
2023-07-18 16:07:50 +0900

    Feat: 모달 외부영역 클릭 시 닫기 - 모달 열고 닫는 부분 하나의 컴포넌트에 두지말고 분리 해야함 - 버튼뿐만 아니라 모든 태그에 대응되도록 수정해야함 - 버튼 컴포넌트에 '테스트 버튼' 텍스트 삭제.   버튼 내부 내용 유동적으로 설정할 수 있게 바꿈

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,24 +1,37 @@
-import { Portal } from "common/Portal";
-import { useRef, useState } from "react";
+import { Portal } from "common";
+import { Button } from "components";
+import { useBoolean, useClickAway } from "hooks";
+import { useRef } from "react";
 import * as Styled from "./Modal.styles";
 
-export const Modal = ({ children }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export const Modal = ({ children, openModalText = "Open", closeModalText = "Close" }) => {
   const innerRef = useRef();
+  const [isOpen, setIsOpen] = useBoolean(false);
 
-  const closeHandler = e => {
-    if (e.currentTarget === e.target) setIsModalOpen(false);
-    console.log("hi");
-    console.log("타겟", e.target);
-    console.log("커런트", e.currentTarget);
+  const openModal = () => {
+    setIsOpen.on();
   };
-  // 타켓, 커런트 타겟 같을때 닫아줘
+
+  const closeModal = () => {
+    setIsOpen.off();
+  };
+
+  useClickAway(innerRef, closeModal);
 
   return (
-    <Portal>
-      <Styled.ModalOuter onClick={closeHandler}>
-        <Styled.ModalInner ref={innerRef}>{children}</Styled.ModalInner>
-      </Styled.ModalOuter>
-    </Portal>
+    <>
+      <Button onClick={openModal}>{openModalText}</Button>
+
+      {isOpen && (
+        <Portal>
+          <Styled.ModalOuter>
+            <Styled.ModalInner ref={innerRef}>
+              {children}
+              <Button onClick={closeModal}>{closeModalText}</Button>
+            </Styled.ModalInner>
+          </Styled.ModalOuter>
+        </Portal>
+      )}
+    </>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,7 @@
 import { Button, Input, Label, Modal, Textarea } from "components";
-
-import { useModal } from "hooks/useModal";
 import { FlexCenter, FlexColumn } from "styles/mixins";
 
 export const Home = () => {
-  const { isOpen, openModal, closeModal } = useModal();
-
   return (
     <div>
       <FlexCenter>
@@ -13,16 +9,11 @@ export const Home = () => {
         <Button variant="outline">버튼</Button>
         <Button size="small">버튼</Button>
         <Button disabled>버튼</Button>
-        <Button size="large" onClick={openModal}>
-          버튼
-        </Button>
+        <Button size="large">버튼</Button>
 
-        {isOpen && (
-          <Modal>
-            <input type="text" placeholder="인풋" />
-            <Button onClick={closeModal}>닫기</Button>
-          </Modal>
-        )}
+        <Modal>
+          <input type="text" placeholder="인풋" />
+        </Modal>
       </FlexCenter>
       <FlexColumn>
         <Label variant="text">아이디</Label>
