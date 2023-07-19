ed7acf1e3880dabc2a78aa8f4a9019f2ceee2471
jeonhaekang <1003gorkd@naver.com>
2023-07-19 09:16:54 +0900

    Design: Home 기본 레이아웃 설정

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import isPropValid from "@emotion/is-prop-valid";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NavermapsProvider } from "react-naver-maps";
 import { BrowserRouter } from "react-router-dom";
 import { StyleSheetManager, ThemeProvider } from "styled-components";
 import { theme } from "styles/theme";
@@ -21,7 +22,9 @@ root.render(
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <BrowserRouter>
-            <App />
+            <NavermapsProvider ncpClientId={process.env.REACT_APP_NAVER_KEY}>
+              <App />
+            </NavermapsProvider>
           </BrowserRouter>
         </ThemeProvider>
       </QueryClientProvider>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,37 +1,19 @@
-import { Button, Input, Label, Textarea } from "components";
-
-import { FlexCenter, FlexColumn } from "styles/mixins";
+import { Header } from "components";
+import { Container, NaverMap } from "react-naver-maps";
+import * as Styled from "./Home.styles";
 
 export const Home = () => {
   return (
-    <div>
-      <FlexCenter>
-        <Button variant="text">버튼</Button>
-        <Button variant="outline">버튼</Button>
-        <Button size="small">버튼</Button>
-        <Button disabled>버튼</Button>
-        <Button size="large">버튼</Button>
-      </FlexCenter>
-      <FlexColumn>
-        <Label variant="text">아이디</Label>
-        <Input variant="outline" placeholder="아이디 입력" />
-        <Label variant="text">닉네임</Label>
-        <Input variant="outline" placeholder="닉네임 입력" />
-        <Label>비밀번호</Label>
-        <Input variant="outline" type="password" placeholder="비밀번호 입력" />
-        <Label>비밀번호 확인</Label>
-        <Input variant="outline" type="password" placeholder="비밀번호 확인" />
-      </FlexColumn>
-      <FlexColumn>
-        <Input placeholder="모임 이름" />
-        <Input placeholder="모임 날짜" />
-        <Input placeholder="모임 장소" />
-        <Input placeholder="모임 오픈톡방" />
-        <Input placeholder="참석 정원" />
-        <Textarea placeholder="모임 소개" />
-      </FlexColumn>
+    <>
+      <Header />
 
-      {/* <Icon name="place" /> */}
-    </div>
+      <Styled.Container>
+        <Styled.Sidebar>사이드바</Styled.Sidebar>
+
+        <Container style={{ flex: 1 }}>
+          <NaverMap />
+        </Container>
+      </Styled.Container>
+    </>
   );
 };
